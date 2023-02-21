664349549ff7713cff4f412c93bb88f875c74c98
Cedric Hazoume <cedric.hazoume@webbfontaine.com>
2023-02-21 08:57:47 +0100

    Initial commit

---
+++ src/main/java/dev/hazoume/movies/ReviewController.java
@@ -0,0 +1,26 @@
+package dev.hazoume.movies;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+/* import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController; */
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/reviews")
+public class ReviewController {
+  
+
+  @Autowired
+  private ReviewService reviewService;
+
+  @PostMapping
+  public ResponseEntity<Review> createreview(@RequestBody Map<String, String> payload) {
+    return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
+  }
+}
